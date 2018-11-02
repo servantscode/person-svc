4932730f81dcadbbccfc70fa2ba94bcea827d4d8
Greg Leitheiser <greg@servantscode.org>
2018-11-02 11:09:05 -0500

    Database pooling with HikariCP

--- src/main/java/org/servantscode/person/db/DBAccess.java
+++ src/main/java/org/servantscode/person/db/DBAccess.java
@@ -1,27 +1,34 @@
 package org.servantscode.person.db;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Date;
 
 public class DBAccess {
 
-    //TODO: Pool this!
-    protected Connection getConnection() {
+    private static HikariDataSource source;
+
+    static {
         try {
+            //Ensure driver is loaded into local context.
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Could not find database driver.");
+            throw new RuntimeException("Database driver not present.", e);
         }
 
-        try {
-            return DriverManager.getConnection(
-                    "jdbc:postgresql://postgres:5432/servantscode", "servant1",
-                    "servant!IsH3r3");
+        source = new HikariDataSource();
+        source.setJdbcUrl("jdbc:postgresql://postgres:5432/servantscode");
+        source.setUsername("servant1");
+        source.setPassword("servant!IsH3r3");
+    }
 
+    protected Connection getConnection() {
+        try {
+            return source.getConnection();
         } catch (SQLException e) {
-            throw new RuntimeException("Could not connect to database.");
+            throw new RuntimeException("Could not connect to database.", e);
         }
     }
 
