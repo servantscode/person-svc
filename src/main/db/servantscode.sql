6cdd8c6e20aad595a9723b0d7d9aa085955d5605
Greg Leitheiser <greg@servantscode.org>
2019-02-01 14:33:41 -0600

    Configurable roles

--- src/main/db/servantscode.sql
+++ src/main/db/servantscode.sql
@@ -2,13 +2,15 @@
 
 CREATE TABLE families (id SERIAL PRIMARY KEY, surname TEXT, addr_street1 TEXT, addr_street2 TEXT, addr_city TEXT, addr_state TEXT, addr_zip INTEGER);
 CREATE TABLE people (id SERIAL PRIMARY KEY, name TEXT, birthdate TIMESTAMP WITH TIME ZONE, email TEXT, phonenumber TEXT, head_of_house boolean, family_id INTEGER, member_since TIMESTAMP WITH TIME ZONE);
-CREATE TABLE logins (person_id INTEGER PRIMARY KEY REFERENCES people(id) ON DELETE CASCADE, hashed_password TEXT, role TEXT);
 CREATE TABLE ministries (id SERIAL PRIMARY KEY, name TEXT, description TEXT);
 CREATE TABLE ministry_enrollments (person_id INTEGER REFERENCES people(id) ON DELETE CASCADE, ministry_id INTEGER REFERENCES ministries(id) ON DELETE CASCADE, role TEXT);
-CREATE TABLE pledges (id SERIAL, family_id INTEGER REFERENCES families(id), pledge_type TEXT, envelope_number INTEGER, pledge_date TIMESTAMP WITH TIME ZONE, pledge_start TIMESTAMP WITH TIME ZONE, pledge_end TIMESTAMP WITH TIME ZONE, frequency TEXT, pledge_increment FLOAT, total_pledge FLOAT);
-CREATE TABLE donations (id BIGSERIAL, family_id INTEGER REFERENCES families(id), amount FLOAT, date TIMESTAMP WITH TIME ZONE, type TEXT, check_number INTEGER, transaction_id bigint);
-CREATE TABLE events (id SERIAL, start_time TIMESTAMP WITH TIME ZONE, recurring_meeting_id INTEGER, end_time TIMESTAMP WITH TIME ZONE, description TEXT, scheduler_id INTEGER REFERENCES people(id), ministry_id INTEGER REFERENCES ministries(id));
-CREATE TABLE rooms (id SERIAL, name TEXT, type TEXT, capacity INTEGER);
-CREATE TABLE equipment (id SERIAL, name TEXT, manufacturer TEXT, description TEXT);
-CREATE TABLE reservations (id BIGSERIAL, resource_type TEXT, resource_id INTEGER, reserving_person_id INTEGER, event_id INTEGER, start_time TIMESTAMP WITH TIME ZONE, end_time TIMESTAMP WITH TIME ZONE);
-CREATE TABLE recurrences(id SERIAL, cycle TEXT, frequency INTEGER, end_date TIMESTAMP WITH TIME ZONE, weekly_days INTEGER);
+CREATE TABLE pledges (id SERIAL PRIMARY KEY, family_id INTEGER REFERENCES families(id), pledge_type TEXT, envelope_number INTEGER, pledge_date TIMESTAMP WITH TIME ZONE, pledge_start TIMESTAMP WITH TIME ZONE, pledge_end TIMESTAMP WITH TIME ZONE, frequency TEXT, pledge_increment FLOAT, total_pledge FLOAT);
+CREATE TABLE donations (id BIGSERIAL PRIMARY KEY, family_id INTEGER REFERENCES families(id), amount FLOAT, date TIMESTAMP WITH TIME ZONE, type TEXT, check_number INTEGER, transaction_id bigint);
+CREATE TABLE events (id SERIAL PRIMARY KEY, start_time TIMESTAMP WITH TIME ZONE, recurring_meeting_id INTEGER, end_time TIMESTAMP WITH TIME ZONE, description TEXT, scheduler_id INTEGER REFERENCES people(id), ministry_id INTEGER REFERENCES ministries(id));
+CREATE TABLE rooms (id SERIAL PRIMARY KEY, name TEXT, type TEXT, capacity INTEGER);
+CREATE TABLE equipment (id SERIAL PRIMARY KEY, name TEXT, manufacturer TEXT, description TEXT);
+CREATE TABLE reservations (id BIGSERIAL PRIMARY KEY, resource_type TEXT, resource_id INTEGER, reserving_person_id INTEGER, event_id INTEGER, start_time TIMESTAMP WITH TIME ZONE, end_time TIMESTAMP WITH TIME ZONE);
+CREATE TABLE recurrences(id SERIAL PRIMARY KEY, cycle TEXT, frequency INTEGER, end_date TIMESTAMP WITH TIME ZONE, weekly_days INTEGER);
+CREATE TABLE roles (id SERIAL PRIMARY KEY, name TEXT);
+CREATE TABLE permissions (role_id INTEGER REFERENCES roles(id) ON DELETE CASCADE, permission TEXT);
+CREATE TABLE logins (person_id INTEGER PRIMARY KEY REFERENCES people(id) ON DELETE CASCADE, hashed_password TEXT, role_id INTEGER REFERENCES roles(id) ON DELETE CASCADE);
